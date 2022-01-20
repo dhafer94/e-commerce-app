79feb025c31d04da6cf86ad2db0efb93e4d37476
Dhafer Alzubairi <dx.dhafer@gmail.com>
2022-01-21 00:03:04 +0200

    Components separation and props passing

---
+++ src/pages/Homepage/Homepage.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Homepage.styles.scss';
+
+import Directory from '../../components/Directory/Directory.component';
+
+const Homepage = () => (
+	<div className='homepage'>
+		<Directory />
+	</div>
+);
+
+export default Homepage;
